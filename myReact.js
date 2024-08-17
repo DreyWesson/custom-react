cd0bd038e7ee164b2309990c506a0a05959dc710
dreywesson <dreywesson@gmail.com>
2024-08-17 13:07:34 +0200

    doesnt crash again but the count init value still got removed

--- myReact.js
+++ myReact.js
@@ -1,5 +1,5 @@
 import { App } from "./App";
-import { createKeyedMap, hasNodeChanged, updateProps } from "./helper";
+
 
 class MyReact {
   constructor() {
@@ -13,25 +13,32 @@ class MyReact {
 
   createElement = (type, props = {}, ...children) => {
     props = props || {};
-  
     const { key = null, ...restProps } = props;
-  
-
-    return (typeof type === 'function') ?type({ ...restProps, children }) : ({
-      type,
-      props: { ...restProps, children: children.flat() },
-      key,
+    
+    const normalizedChildren = children.flat().map((child, index) => {
+      if (typeof child === 'object' && child !== null && !child.key) {
+        child.key = index;  // Assigning a key based on index if not provided
+      }
+      return child;
     });
+
+    return typeof type === 'function'
+      ? type({ ...restProps, children: normalizedChildren })
+      : {
+          type,
+          props: { ...restProps, children: normalizedChildren },
+          key,
+        };
   };
-  
-  
 
   createRealDOM = (element) => {
-    if (typeof element === "string" || typeof element === "number")
+    if (typeof element === "string" || typeof element === "number") {
       return document.createTextNode(String(element));
+    }
 
-    if (!element || !element.type)
+    if (!element || !element.type) {
       return document.createTextNode('');
+    }
 
     if (typeof element.type === 'function') {
       const renderedElement = element.type(element.props);
@@ -42,7 +49,7 @@ class MyReact {
     const domElement = document.createElement(element.type);
     updateProps(domElement, {}, props);
     this.appendChildren(domElement, children);
-    
+
     return domElement;
   };
 
@@ -52,9 +59,7 @@ class MyReact {
       .map(this.createRealDOM)
       .forEach((child) => domElement.appendChild(child));
   };
-  
 
-  // 2. Rendering the Virtual DOM in the container/root
   renderToDOM = (newVDOM, root) => {
     this.container = root;
 
@@ -66,7 +71,6 @@ class MyReact {
     return this;
   };
 
-  // 3. Managing State
   useState = (initialValue) => {
     const idx = this.stateIdx;
 
@@ -77,87 +81,50 @@ class MyReact {
 
     const setState = (newValue) => {
       this.state[idx] =
-        (typeof newValue === "function") ? newValue(this.state[idx]) : newValue;
+        typeof newValue === "function" ? newValue(this.state[idx]) : newValue;
       this.processUpdate();
     };
-    console.log(`Rendering: idx=${idx}, state=`, this.state);
     this.stateIdx++;
     return [this.state[idx], setState];
   };
 
-  // 4. Diffing and Updating the DOM
-//   diff = (parent, oldNode, newNode, index = 0) => {
-//     const existingNode = parent.childNodes[index];
+  diff = (parent, oldNode, newNode, index = 0) => {
+    const existingNode = parent.childNodes[index];
 
-//     if (!newNode) {
-//         return existingNode && parent.removeChild(existingNode);
-//     }
-
-//     if (!oldNode) {
-//         return parent.appendChild(this.createRealDOM(newNode));
-//     }
-
-//     if (hasNodeChanged(oldNode, newNode)) {
-//         const newDomNode = this.createRealDOM(newNode);
-//         if (existingNode && newDomNode) {
-//             return parent.replaceChild(newDomNode, existingNode);
-//         } else if (newDomNode) {
-//             return parent.appendChild(newDomNode);
-//         }
-//     }
-
-//     if (newNode.type) {
-//         updateProps(existingNode, oldNode.props, newNode.props);
-//         this.diffChildren(
-//             existingNode,
-//             oldNode.props.children,
-//             newNode.props.children
-//         );
-//     }
-// };
-
-diff = (parent, oldNode, newNode, index = 0) => {
-  const existingNode = parent.childNodes[index];
-
-
-  if (!newNode) {
+    if (!newNode) {
       return existingNode && parent.removeChild(existingNode);
-  }
+    }
 
-  if (!oldNode) {
+    if (!oldNode) {
       return parent.appendChild(this.createRealDOM(newNode));
-  }
+    }
 
-  // Handling text nodes (numbers and strings)
-  if (typeof oldNode === "string" || typeof oldNode === "number") {
+    if (typeof oldNode === "string" || typeof oldNode === "number") {
       if (typeof newNode === "string" || typeof newNode === "number") {
-          if (existingNode.textContent !== String(newNode)) {
-              existingNode.textContent = String(newNode);
-          }
-          return;
+        if (existingNode && existingNode.textContent !== String(newNode)) {
+          existingNode.textContent = String(newNode);
+        }
+        return;
       } else {
-          const newDomNode = this.createRealDOM(newNode);
-          return parent.replaceChild(newDomNode, existingNode);
+        const newDomNode = this.createRealDOM(newNode);
+        return parent.replaceChild(newDomNode, existingNode);
       }
-  }
+    }
 
-  if (hasNodeChanged(oldNode, newNode)) {
+    if (hasNodeChanged(oldNode, newNode)) {
       const newDomNode = this.createRealDOM(newNode);
       return parent.replaceChild(newDomNode, existingNode);
-  }
+    }
 
-  if (newNode.type) {
+    if (newNode.type) {
       updateProps(existingNode, oldNode.props, newNode.props);
       this.diffChildren(
-          existingNode,
-          oldNode.props.children,
-          newNode.props.children
+        existingNode,
+        oldNode.props.children,
+        newNode.props.children
       );
-  }
-};
-
-
-
+    }
+  };
 
   diffChildren = (parent, oldChildren = [], newChildren = []) => {
     const oldChildrenKeyed = createKeyedMap(oldChildren);
@@ -223,8 +190,6 @@ diff = (parent, oldNode, newNode, index = 0) => {
     this.isRendering = false;
     this.stateIdx = 0;
   };
-
-
 }
 
 const myReactInstance = new MyReact();
@@ -232,3 +197,107 @@ const myReactInstance = new MyReact();
 export const createElement = myReactInstance.createElement;
 export const useState = myReactInstance.useState;
 export const render = myReactInstance.render;
+
+export const createKeyedMap = (children) => {
+  return children.reduce((acc, child, index) => {
+    if (child && child.key != null) acc[child.key] = child;
+    else acc[index] = child;
+    return acc;
+  }, {});
+};
+
+export const hasNodeChanged = (node1, node2) => {
+  return (
+    typeof node1 !== typeof node2 ||
+    ((typeof node1 === "string" || typeof node1 === "number") &&
+      node1 !== node2) ||
+    node1.type !== node2.type ||
+    node1.key !== node2.key
+  );
+};
+
+export const updateProps = (domElement, oldProps, newProps) => {
+  removeOldProps(domElement, oldProps, newProps);
+  addNewProps(domElement, oldProps, newProps);
+};
+
+export const removeOldProps = (domElement, oldProps, newProps) => {
+  for (let name in oldProps) {
+    if (name === "children") continue;
+
+    if (name === "style") {
+      const oldStyle = oldProps[name] || {};
+      const newStyle = newProps[name] || {};
+      for (let styleName in oldStyle) {
+        if (!(styleName in newStyle)) {
+          domElement.style[styleName] = "";
+        }
+      }
+    } else if (!(name in newProps)) {
+      removeProp(domElement, name, oldProps[name]);
+    }
+  }
+};
+
+export const addNewProps = (domElement, oldProps, newProps) => {
+  for (let name in newProps) {
+    if (name === "children") continue;
+
+    if (name === "style") {
+      const oldStyle = oldProps[name] || {};
+      const newStyle = newProps[name] || {};
+      for (let styleName in newStyle) {
+        let styleValue = newStyle[styleName];
+        
+        if (typeof styleValue === 'function') {
+          styleValue = styleValue();
+        }
+
+        if (isPropChanged(oldStyle[styleName], styleValue)) {
+          domElement.style[styleName] = styleValue;
+        }
+      }
+    } else if (isPropChanged(oldProps[name], newProps[name])) {
+      setProp(domElement, name, newProps[name]);
+    }
+  }
+};
+
+export const removeProp = (domElement, name, value) => {
+  if (name.startsWith("on")) {
+    const eventType = name.toLowerCase().substring(2);
+    domElement.removeEventListener(eventType, value);
+  } else if (name === "style") {
+    domElement.style = "";
+  } else if (name in domElement) {
+    domElement[name] = "";
+  } else {
+    domElement.removeAttribute(name);
+  }
+};
+
+export const setProp = (domElement, name, value) => {
+  if (name.startsWith("on")) {
+    const eventType = name.toLowerCase().substring(2);
+
+    const oldValue = domElement._eventListeners && domElement._eventListeners[name];
+    if (oldValue) {
+      domElement.removeEventListener(eventType, oldValue);
+    }
+
+    domElement.addEventListener(eventType, value);
+
+    domElement._eventListeners = domElement._eventListeners || {};
+    domElement._eventListeners[name] = value;
+  } else if (name === "style") {
+    domElement.style.cssText = value;
+  } else if (name in domElement) {
+    domElement[name] = value;
+  } else {
+    domElement.setAttribute(name, value);
+  }
+};
+
+export const isPropChanged = (oldValue, newValue) => {
+  return oldValue !== newValue;
+};
